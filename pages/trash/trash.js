39be5c2f7cf9820f5ed122f47394fd24149744f6
zhao_ww <zhao_ww@murongtech.com>
2019-09-25 10:49:08 +0800

    1.0.0
    1.可以全名搜索，不支持模糊搜索
    2.数据有限

--- pages/trash/trash.js
+++ pages/trash/trash.js
@@ -1,12 +1,30 @@
 Page({
   data:{
-    searchValue:"塑料垃圾桶",
+    searchValue:"塑料袋",
     trashType:"请搜索"
   },
   btnclick: function(){
     var localData = require("../../data/searchInfo.js")
-    console.log("请求内容：" + localData.testJsonList)
-    console.log("搜索内容：" + searchValue)
+    var arr = localData.testJsonList;
+    for(let i=0;i<arr.length;i++){
+      console.log(this.data.searchValue + ":" + arr[i].key)
+      
+      if (this.data.searchValue == arr[i].key) {
+        console.log(arr[i].value)
+        this.setData({
+          trashType: arr[i].value
+        })
+      }
+    }
+
+    console.log("请求内容：" + localData.testJsonList);
+    console.log("搜索内容：" + this.data.searchValue)
     
+  },
+  searchInput: function(e) {
+    var val = e.detail.value;
+    this.setData({
+      searchValue:val
+    })
   }
 })
